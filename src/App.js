bb61e19ba741cf2bea473fce17f0eb3115d775a2
bahainblock <baha_prog2kurs@mail.ru>
2023-09-14 01:15:11 +0600

    created 3 important components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
+
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 
 const App = () => {
@@ -14,9 +18,13 @@ const App = () => {
             <Header />
             <Navbar />
             <div className = 'app-wrapper-content'>
+
                 <Routes>
                     <Route path= "/Dialogs" element= {<Dialogs/>}/>
                     <Route path= "/Profile" element= {<Profile/>}/>
+                    <Route path= "/News" element= {<News/>}/>
+                    <Route path='Music' element={<Music/>}/>
+                    <Route path='Settings' element={<Settings/>}/>
                 </Routes>
             </div>
         </div>
