b2b628274c0349cfef78c281270414e0e824a942
Baha <baha_prog2kurs@mail.ru>
2023-09-25 18:10:51 +0600

    laid out the dialogue page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,29 @@ import React from "react";
 import s from './Dialogs.module.css';
 const Dialogs = (react) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    Baha
+                </div>
+                <div className={s.dialog}>
+                    Kirill
+                </div>
+                <div className={s.dialog}>
+                    Alihan
+                </div>
+                <div className={s.dialog}>
+                    Dani
+                </div>
+                <div className={s.dialog}>
+                    IX
+                </div>
+                    <div className={s.messages}>
+                    <div className={s.message}>Salam</div>
+                    <div className={s.message}>Aym</div>
+                    <div className={s.message}>Asal</div>
+                </div>
+            </div>
         </div>
     )
 }
