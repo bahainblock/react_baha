6dbb1f0b55927168514e45ff3daf2738536f0081
bahainblock <baha_prog2kurs@mail.ru>
2023-09-26 00:02:14 +0600

    create new users and sverstal pochti vce

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,39 @@
 import React from "react";
 import s from './Dialogs.module.css';
-const Dialogs = (react) => {
+const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className= {s.dialogsItems}>
+                <div className={s.dialog}>
+                    Danka
+                </div>
+                <div className={s.dialog}>
+                    Alina
+                </div>
+                <div className={s.dialog}>
+                    Alihan
+                </div>
+                <div className={s.dialog}>
+                    Sister
+                </div>
+                <div className={s.dialog}>
+                    Kirill
+                </div>
+                <div className={s.dialog}>
+                    Mama
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hi brr
+                </div>
+                <div className={s.message}>
+                    Yoo
+                </div>
+                <div className={s.message}>
+                    My name is
+                </div>
+            </div>
         </div>
     )
 }
